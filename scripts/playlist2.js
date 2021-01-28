701f07c8da01d87d061dcc1b138c97c563da85f7
jeremy51307 <jeremy51307@yahoo.com.tw>
2021-01-28 23:06:46 +0800

    add

--- scripts/playlist2.js
+++ scripts/playlist2.js
@@ -7,7 +7,7 @@
         {
 		name:"Asiaboy 禁藥王 Lizi 栗子 - 都關掉 Feat. 蛋頭 BG8Locc",
 		artist:"Asiaboy",
-		src:"music/Asiaboy 禁藥王 Lizi 栗子 - 都關掉 Feat. 蛋頭 BG8Locc.mp3"
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 都關掉 Feat 蛋頭 BG8Locc.mp3"
         },
 	{
 		name:"Asiaboy - 藥王回來到",
@@ -17,7 +17,7 @@
         {
 		name:"Asiaboy - Everybody Fake Feat. 婁峻碩SHOU",
 		artist:"Asiaboy",
-		src:"music/Asiaboy - Everybody Fake Feat. 婁峻碩SHOU.mp3"
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 Everybody Fake Feat 婁峻碩SHOU .mp3"
         },
 	{
 		name:"Asiaboy 禁藥王 - 拖鞋 feat. Sowut.mp3",
@@ -34,12 +34,7 @@
 		artist:"Asiaboy",
 		src:"music/莫宰羊 - 國王 King (Cypher) ft. Asiaboy Lizi Gambler .mp3"
 	},
-        {
-		name:"Asiaboy 禁藥王 - Swift16 (Prod. By Starr Chen)",
-		artist:"Asiaboy",
-		src:"music/Asiaboy 禁藥王 - Swift16 (Prod. By Starr Chen) .mp3"
-        },
-	{
+       	{
 		name:"華興-周湯豪Nickthereal婁峻碩SHOU蛋頭BG8LOCC禁藥王Sowut",
 		artist:"Asiaboy",
 		src:"music/華興-周湯豪Nickthereal婁峻碩SHOU蛋頭BG8LOCC禁藥王Sowut.mp3"
