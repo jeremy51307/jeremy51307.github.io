0c381a9b63f52a6ff2eb0fc4f36a7dae2c91b214
jeremy51307 <jeremy51307@yahoo.com.tw>
2021-01-28 22:50:39 +0800

    add new song for asiaboy

--- scripts/playlist2.js
+++ scripts/playlist2.js
@@ -1,7 +1,47 @@
 ﻿trackList = [
 	{
-		name:"禁藥王-wake up in the moring",
-		artist:"禁藥王",
-		src:"music/禁藥王-WAKE UP IN THE MORNING.mp3"
+		name:"Asiaboy-wake up in the moring",
+		artist:"Asiaboy",
+		src:"music/Asiaboy-WAKE UP IN THE MORNING.mp3"
+	},
+        {
+		name:"Asiaboy 禁藥王 Lizi 栗子 - 都關掉 Feat. 蛋頭 BG8Locc",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 - 都關掉 Feat. 蛋頭 BG8Locc.mp3"
+        },
+	{
+		name:"Asiaboy - 藥王回來到",
+		artist:"Asiaboy",
+		src:"music/Asiaboy - 藥王回來到.mp3"
+	},
+        {
+		name:"Asiaboy - Everybody Fake Feat. 婁峻碩SHOU",
+		artist:"Asiaboy",
+		src:"music/Asiaboy - Everybody Fake Feat. 婁峻碩SHOU.mp3"
+        },
+	{
+		name:"Asiaboy 禁藥王 - 拖鞋 feat. Sowut.mp3",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 - 拖鞋 feat. Sowut.mp3"
+	},
+        {
+		name:"Asiaboy 禁藥王 Lizi 栗子 - PIMP",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 - PIMP.mp3"
+        },
+	{
+		name:"莫宰羊 - 國王 King (Cypher) ft. Asiaboy Lizi Gambler",
+		artist:"Asiaboy",
+		src:"music/莫宰羊 - 國王 King (Cypher) ft. Asiaboy Lizi Gambler .mp3"
+	},
+        {
+		name:"Asiaboy 禁藥王 - Swift16 (Prod. By Starr Chen)",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 - Swift16 (Prod. By Starr Chen) .mp3"
+        },
+	{
+		name:"華興-周湯豪Nickthereal婁峻碩SHOU蛋頭BG8LOCC禁藥王Sowut",
+		artist:"Asiaboy",
+		src:"music/華興-周湯豪Nickthereal婁峻碩SHOU蛋頭BG8LOCC禁藥王Sowut.mp3"
 	}
 ];
