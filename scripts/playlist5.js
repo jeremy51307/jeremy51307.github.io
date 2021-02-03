67477f4969f8937c7d1f125bcbc5d29d922ec0b9
jeremy51307 <jeremy51307@yahoo.com.tw>
2021-02-04 00:10:07 +0800

    zayn playlist

---
+++ scripts/playlist5.js
@@ -0,0 +1,332 @@
+﻿trackList = [
+	{
+		name:"Harry Styles She Official Audio",
+		artist:"harry styles",
+		src:"zayn/yt1s.com - Harry Styles She Official Audio.mp3"
+	},
+	{
+		name:"Justin Bieber Anyone",
+		artist:"Justin Bieber",
+		src:"zayn/yt1s.com - Justin Bieber Anyone.mp3"
+	},
+	{
+		name:"Louis Tomlinson Defenceless Official Lyric Video",
+		artist:"Louis Tomlinson",
+		src:"zayn/yt1s.com - Louis Tomlinson Defenceless Official Lyric Video.mp3"
+	},
+	{
+		name:"Major Lazer Cold Water feat Justin Bieber MØ Official Dance Video",
+		artist:"Major Lazer",
+		src:"zayn/yt1s.com - Major Lazer Cold Water feat Justin Bieber MØ Official Dance Video.mp3"
+	},
+	{
+		name:"One Direction 18 Audio",
+		artist:"One Direction",
+		src:"zayn/yt1s.com - One Direction 18 Audio.mp3"
+	},
+	{
+		name:"One Direction Best Song Ever",
+		artist:"One Direction",
+		src:"zayn/yt1s.com - One Direction Best Song Ever.mp3"
+	},
+	{
+		name:"One Direction History Official Video",
+		artist:"One Direction",
+		src:"zayn/yt1s.com - One Direction History Official Video.mp3"
+	},
+	{
+		name:"One Direction You I",
+		artist:"One Direction",
+		src:"zayn/yt1s.com - One Direction You I.mp3"
+	},
+	{
+		name:"One Direction Little Things",
+		artist:"One Direction",
+		src:"zayn/yt1s.com - One Direction Little Things.mp3"
+	},
+	{
+		name:"R3HAB ZAYN Jungleboi Flames Official Music Video",
+		artist:"ZAYN",
+		src:"zayn/yt1s.com - R3HAB ZAYN Jungleboi Flames Official Music Video.mp3"
+	},
+	{
+		name:"Snakehips Cruel Official Video ft ZAYN",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - Snakehips Cruel Official Video ft ZAYN.mp3"
+	},
+	{
+		name:"ZAYN All That Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN All That Audio.mp3"
+	},
+	{
+		name:"ZAYN Back To Life Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Back To Life Audio.mp3"
+	},
+	{
+		name:"ZAYN Becky G Un mundo ideal Versión Créditos De AladdínOfficial Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Becky G Un mundo ideal Versión Créditos De AladdínOfficial Video.mp3"
+	},
+	{
+		name:"ZAYN BeFoUr",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN BeFoUr.mp3"
+	},
+	{
+		name:"ZAYN BLUE",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN BLUE Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN BoRdErSz",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN BoRdErSz Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN BRIGHT Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN BRIGHT Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Calamity Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Calamity Audio.mp3"
+	},
+{
+		name:"Zayn Cant Help Falling in Love Cover",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - Zayn Cant Help Falling in Love Cover.mp3"
+	},
+	{
+		name:"ZAYN Back To Life Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Back To Life Audio.mp3"
+	},
+	{
+		name:"ZAYN Common Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Common Audio.mp3"
+	},
+	{
+		name:"ZAYN Connexion Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Connexion Audio.mp3"
+	},
+	{
+		name:"ZAYN dRuNk Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN dRuNk Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Dusk Till Dawn ft Sia Official Music Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Dusk Till Dawn ft Sia Official Music Video.mp3"
+	},
+	{
+		name:"ZAYN Entertainer Official Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Entertainer Official Video.mp3"
+	},
+	{
+		name:"ZAYN Fingers Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Fingers Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Flight Of The Stars Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Flight Of The Stars Audio.mp3"
+	},
+	{
+		name:"ZAYN fOoL fOr YoU Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN fOoL fOr YoU Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Fresh Air Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Fresh Air Audio.mp3"
+	},
+	{
+		name:"ZAYN Good Guy Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Good Guy Audio.mp3"
+	},
+	{
+		name:"ZAYN Good Years Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Good Years Audio.mp3"
+	},
+	{
+		name:"ZAYN I Dont Mind Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN I Dont Mind Audio.mp3"
+	},
+	{
+		name:"ZAYN If I Got You Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN If I Got You Audio.mp3"
+	},
+	{
+		name:"ZAYN Imprint Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Imprint Audio.mp3"
+	},
+	{
+		name:"ZAYN Insomnia Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Insomnia Audio.mp3"
+	},
+	{
+		name:"ZAYN INTERMISSION fLoWer Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN INTERMISSION fLoWer Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN iTs YoU",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN iTs YoU.mp3"
+	},
+	{
+		name:"ZAYN Let Me Official Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Let Me Official Video.mp3"
+	},
+	{
+		name:"ZAYN LIKE I WOULD",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN LIKE I WOULD.mp3"
+	},
+	{
+		name:"ZAYN lUcOzAdE Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN lUcOzAdE Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Natural Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Natural Audio.mp3"
+	},
+	{
+		name:"ZAYN Outside Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Outside Audio.mp3"
+	},
+	{
+		name:"ZAYN PILLOWTALK Official Music Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN PILLOWTALK Official Music Video.mp3"
+	},
+	{
+		name:"ZAYN Rainberry Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Rainberry Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN rEaR vIeW Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN rEaR vIeW Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN River Road Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN River Road Audio.mp3"
+	},
+	{
+		name:"ZAYN Satisfaction Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Satisfaction Audio.mp3"
+	},
+	{
+		name:"ZAYN Scripted Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Scripted Audio.mp3"
+	},
+	{
+		name:"ZAYN SHE DONT LOVE ME Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN SHE DONT LOVE ME Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN sHe Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN sHe Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Sour Diesel",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Sour Diesel.mp3"
+	},
+	{
+		name:"ZAYN Stand Still Official Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Stand Still Official Video.mp3"
+	},
+	{
+		name:"ZAYN Sweat Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Sweat Audio.mp3"
+	},
+	{
+		name:"ZAYN Talk To Me Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Talk To Me Audio.mp3"
+	},
+	{
+		name:"ZAYN Taylor Swift I Dont Wanna Live Forever Fifty Shades Darker",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Taylor Swift I Dont Wanna Live Forever Fifty Shades Darker.mp3"
+	},
+	{
+		name:"ZAYN There You Are Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN There You Are Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN TiO Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN TiO Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Too Much ft Timbaland",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Too Much ft Timbaland.mp3"
+	},
+	{
+		name:"ZAYN tRuTh Lyric Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN tRuTh Lyric Video.mp3"
+	},
+	{
+		name:"ZAYN Unfkwitable Lyrics",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Unfkwitable Lyrics.mp3"
+	},
+	{
+		name:"ZAYN Vibez Official Video",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Vibez Official Video.mp3"
+	},
+	{
+		name:"ZAYN When Loves Around Audio ft Syd",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN When Loves Around Audio ft Syd.mp3"
+	},
+	{
+		name:"ZAYN Windowsill Audio ft Devlin",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN Windowsill Audio ft Devlin.mp3"
+	},
+	{
+		name:"ZAYN wRoNg Lyric Video ft Kehlani",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN wRoNg Lyric Video ft Kehlani.mp3"
+	},
+	{
+		name:"ZAYN You Wish You Knew Audio",
+		artist:"Zayn",
+		src:"zayn/yt1s.com - ZAYN You Wish You Knew Audio.mp3"
+	}
+];
